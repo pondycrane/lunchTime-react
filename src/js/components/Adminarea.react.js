d4ca6dd082b9ae4d24be250f912332184f892147
pondycrane <pondycrane@gmail.com>
2015-06-14 20:55:59 +0800

    add file upload and replace current menu

--- src/js/components/Adminarea.react.js
+++ src/js/components/Adminarea.react.js
@@ -1,18 +1,79 @@
 var React = require('react');
 
+firebaseStoresRef = new Firebase("https://lunchwhat.firebaseio.com/Stores/");
+
+var StoreListing = React.createClass({
+	render: function(){
+		return <option value={this.props.storeName}>{this.props.storeName}</option>
+	}
+})
+
 var Adminarea = React.createClass({
+	mixins: [ReactFireMixin],
+	componentWillMount: function() {
+		this.bindAsObject(firebaseStoresRef, "stores"); 
+	},
+	getInitialState: function() {
+		return {stores:{}}
+	},
 	addCredit: function(event) {
 		event.preventDefault();
 		if (typeof this.props.addCredit === 'function') {
 			this.props.addCredit();
 		}
 	},
+	pickin: function(event) {
+		event.preventDefault(); 
+		filepicker.setKey('AiD66Zr4PS4aUqLQzYQmbz');
+		filepicker.pickAndStore(
+		  {
+			openTo: '/Imagesearch/lolcats'
+		  },
+		  {},
+		  function(Blobs){
+			console.log(JSON.stringify(Blobs));
+			buchi = Blobs[0].url.split('/'); 
+			  thisPush = {
+				storeName: document.getElementById('storeNameInput').value, 
+				phone: document.getElementById('storePhoneInput').value, 
+				address: document.getElementById('storeAddressInput').value,
+				menu: [], 
+				filePickerKey: [buchi[buchi.length-2], buchi[buchi.length-1]].join('/')
+			  }
+			  restaurantRef = new Firebase("https://lunchwhat.firebaseio.com/Stores/"+thisPush.storeName);
+			  restaurantRef.set(thisPush); 
+			  currentRestaurantRef = new Firebase("https://lunchwhat.firebaseio.com/nowRestaurant/");
+			  currentRestaurantRef.set(thisPush); 
+		  },
+		  function(error){
+			console.log(JSON.stringify(error));
+		  },
+		  function(progress){
+			  console.log(JSON.stringify(progress));
+		  }
+		);
+	}, 
 	render: function() {
+		storeShowcase = []
+		for (storeName in this.state.stores) {
+			storeShowcase.push(<StoreListing storeName={storeName}/>); 
+		}
 		return (
-			<form>
-			  <input id="creditAdd" placeholder="Amount"></input>
-			  <button onClick={this.addCredit} type="submit">Add order</button>
-			</form>
+			<div id="Adminarea">
+				<select>
+					{storeShowcase}
+				</select>
+				<form>
+					<input id='storeNameInput' type="text" placeholder="Store Name"></input>
+					<input id='storePhoneInput' type="text" placeholder="Phone"></input>
+					<input id='storeAddressInput' type="text" placeholder="Address"></input>
+					<button onClick={this.pickin}>Upload File</button>
+				</form>
+				<form>
+				  <input id="creditAdd" placeholder="Amount"></input>
+				  <button onClick={this.addCredit} type="submit">Add order</button>
+				</form>
+			</div>
 		)
 	}
 });
--- src/js/components/FilePickerMenu.react.js
+++ src/js/components/FilePickerMenu.react.js
@@ -1,11 +1,23 @@
 var React = require('react');
 
+currentRestaurantRef = new Firebase("https://lunchwhat.firebaseio.com/nowRestaurant/");
 
 var FilePickerMenu = React.createClass({
+	mixins: [ReactFireMixin],
+	componentWillMount: function() {
+		this.bindAsObject(currentRestaurantRef, "nowStore"); 
+	},
+	getInitialState: function() {
+		return {
+			nowStore: {
+				filePickerKey:''
+			}
+		}
+	}, 
   render: function(){
     return (
       <div id="FilePickerMenu">
-        <img src={"https://www.filepicker.io/api/file/Q8dSRRUQQumUfcohmaXK"} alt={"Delicious"} height={"400"} width={"400"}/>
+        <img src={"https://www.filepicker.io/api/file/"+this.state.nowStore.filePickerKey} alt={"Delicious"} height={"400"} width={"400"}/>
       </div>
     )
   }
